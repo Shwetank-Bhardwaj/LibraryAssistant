10054a92179eb21b0e8dc464976de8dee2ea14ab
ShwetankASU <sbhard18@asu.edu>
2019-11-25 20:48:02 -0700

    Apache jena  library support added

--- app/src/main/java/com/shwetank/libraryassistant/network/NetworkManagerImpl.java
+++ app/src/main/java/com/shwetank/libraryassistant/network/NetworkManagerImpl.java
@@ -3,15 +3,15 @@ package com.shwetank.libraryassistant.network;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
 import com.shwetank.libraryassistant.ArtData;
 import com.shwetank.libraryassistant.ArtistData;
 import com.shwetank.libraryassistant.model.Art;
 import com.shwetank.libraryassistant.model.ArtBeaconList;
 import com.shwetank.libraryassistant.model.Artist;
-
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.ResultSet;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -70,11 +70,58 @@ public class NetworkManagerImpl implements NetworkManager {
 
         @Override
         protected Void doInBackground(Void... voids) {
+            beaconList.add("41");
+            for (String s : beaconList) {
+                String query = "PREFIX cidoccrm: <http://www.cidoc-crm.org/cidoc-crm/>\n" +
+                        "PREFIX saam: <http://edan.si.edu/saam/id/ontologies/>\n" +
+                        "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                        "PREFIX ds2: <http://ec2-54-153-4-119.us-west-1.compute.amazonaws.com:3030/ds2/>\n" +
+                        "PREFIX ds3: <http://ec2-54-153-122-152.us-west-1.compute.amazonaws.com:3030/ds3/>\n" +
+                        "SELECT ?artwork ?artworkurl ?artworkname ?artworkinfo ?artist ?artistfirstname ?artistlastname ?artisturl ?artistbio ?artistnationality ?artistbirthdatelabel ?artistdeathdatelabel ?artworktype ?artworkclassification ?artworkclassification2 ?currentkeeper ?artworkmedium\n" +
+                        "WHERE {\n" +
+                        "  ?location ?label \"109123\" .\n" +
+                        "  ?artwork cidoccrm:P55_has_current_location ?location .\n" +
+                        "  SERVICE ds2:sparql {\n" +
+                        "    ?artwork cidoccrm:P138i_has_representation ?artworkurl .\n" +
+                        "    ?artwork cidoccrm:P102_has_title ?artworktitle .\n" +
+                        "  \t?artworktitle rdfs:label ?artworkname .\n" +
+                        "    ?artwork saam:PE_has_note_gallerylabel ?artworkinfo .\n" +
+                        "  \t?producedby cidoccrm:P108_has_produced ?artwork .\n" +
+                        "    ?producedby cidoccrm:P14_carried_out_by ?artist .\n" +
+                        "    #Recommendation filering attributes\n" +
+                        "  \t?artwork rdf:type ?artworktype .\n" +
+                        "  \t?artwork saam:PE_object_mainclass ?artworkclassification .\n" +
+                        "  \tOPTIONAL { ?artwork saam:PE_object_subsclass ?artworkclassification2 . }\n" +
+                        "  \t?artwork cidoccrm:P50_has_current_keeper ?currentkeeper .\n" +
+                        "  \t?artwork saam:PE_medium_description ?artworkmedium .\n" +
+                        "  }\n" +
+                        "  SERVICE ds3:query {\n" +
+                        "  \t?artist saam:PE_has_note_luceartistbio ?artistbio .\n" +
+                        "    ?artist cidoccrm:P107i_is_current_or_former_member_of ?artistnatiionalityobj .\n" +
+                        "  \t?artistnatiionalityobj skos:prefLabel ?artistnationality .\n" +
+                        "  \t?artist cidoccrm:P98i_was_born ?artistbirthdateobj .\n" +
+                        "  \t?artistbirthdateobj cidoccrm:P4_has_time-span ?artistbirthdate .\n" +
+                        "  \t?artistbirthdate cidoccrm:P82_at_some_time_within ?artistbirthdatelabel .\n" +
+                        "  \t?artist cidoccrm:P100i_died_in ?artistdeathdateobj .\n" +
+                        "  \t?artistdeathdateobj cidoccrm:P4_has_time-span ?artistdeathdate .\n" +
+                        "  \t?artistdeathdate cidoccrm:P82_at_some_time_within ?artistdeathdatelabel .\n" +
+                        "  \t?artist cidoccrm:P1_is_identified_by ?displayname .\n" +
+                        "  \t?displayname saam:PE_firstname ?artistfirstname .\n" +
+                        "  \t?displayname saam:PE_lastname ?artistlastname .\n" +
+                        "  \t?artist cidoccrm:P138i_has_representation ?artisturl .\n" +
+                        "  }\n" +
+                        "  FILTER (contains(str(?displayname), \"displayname\"))\n" +
+                        "  FILTER NOT EXISTS {\n" +
+                        "  \tFILTER (contains(str(?artworkurl), \"jpg\"))\n" +
+                        "  }\n" +
+                        "}";
+                QueryExecution queryExecution = QueryExecutionFactory.sparqlService("http://ec2-54-193-127-210.us-west-1.compute.amazonaws.com:3030/ds1/query", query);
+                ResultSet resultSet = queryExecution.execSelect();
+            }
+
             ArtBeaconList artBeaconList = new ArtBeaconList(beaconList);
-            /*
-            QueryExecution queryExecution = QueryExecutionFactory.sparqlService("endpoint", "query");
-            ResultSet resultSet = queryExecution.execSelect();
-            */
             mRetrofit.create(GetDataService.class).getBulkArt(artBeaconList).enqueue(new Callback<List<Art>>() {
                 @Override
                 public void onResponse(Call<List<Art>> call, Response<List<Art>> response) {
@@ -133,9 +180,10 @@ public class NetworkManagerImpl implements NetworkManager {
             Artist artist = new Artist();
             artist.setBirthDate(new Date().toString().substring(0, 10));
             artist.setNationality("American");
-            artist.setArtistDescription("Leonardo da Vinci was a Renaissance painter, sculptor, architect, inventor, military engineer and draftsman — the epitome of a true Renaissance man. Gifted with a curious mind and a brilliant intellect, da Vinci studied the laws of science and nature, which greatly informed his work. His drawings, paintings and other works have influenced countless artists and engineers over the centuries. ");
-            artist.setImageUrl("https://ids.si.edu/ids/deliveryService?id=SAAM-1991.183_1");
+            artist.setArtistDescription("Frank Blackwell Mayer was born into a life of privilege. His father was a lawyer who represented Baltimore in the Maryland State Senate, and his mother was the daughter of a merchant ship captain. Mayer established the Artists Association of Maryland and remained active in Baltimore’s art community throughout his career. He lived in Paris during the Civil War, returning home to discover that the war had destroyed the city’s art community and sapped his family’s finances. In December 1876 he bought a house in Annapolis and hung a sign above the door bearing the motto: “He lives best who lives well in obscurity.” (Page, “Francis Blackwell Mayer,” <i>The Magazine Antiques</i>, February 1976)");
+            artist.setImageUrl("https://2.americanart.si.edu/images/luce/artists/portrait_image_113230.jpg");
             artist.setArtistName(artistName);
+            artist.setBirthDate("1827/12/27");
             artistData.artistData(artist);
         }
     }
